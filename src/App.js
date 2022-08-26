3a590f3ba1c8dc3ebce48a3e279ffbff952317f3
Desy Kristianti <desy.kristianti@prolific.co>
2022-08-26 11:43:46 +0100

    Add top bar and bottom navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import { BottomNavigation, BottomNavigationAction } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+import PersonIcon from '@mui/icons-material/Person';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+const App = () => {
+  const [nav, setNav] = React.useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar className="logo-container" position="static" color="transparent">
+      <img src={logo} className="logo" alt="SMOKoes logo" />
+      </AppBar>
+      <BottomNavigation value={nav} onChange={(_e, newValue) => {
+        setNav(newValue);
+      }} className="bottom-nav" showLabels>
+        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Leaderboard" icon={<LeaderboardIcon />} />
+        <BottomNavigationAction label="My Profile" icon={<PersonIcon />} />
+      </BottomNavigation>
     </div>
   );
 }
